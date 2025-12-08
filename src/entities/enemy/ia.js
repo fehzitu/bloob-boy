c1476a8f9ffaa4ee3cdd6ba52eca43054d9cd94d
fehzitu <fehzitujsdev@gmail.com>
2025-12-08 17:49:11 -0300

    fix enemy flip sprite

--- src/entities/enemy/ia.js
+++ src/entities/enemy/ia.js
@@ -2,9 +2,9 @@ import { k } from '../../kaboomLoader.js';
 
 export function addEnemyIa(enemy, style) {
     // verify if the enemy exist
-    if(!enemy.exists()) return;
+    if (!enemy.exists()) return;
     // verify ia movement style
-    if(style == 'simple') {
+    if (style == 'simple') {
         // make enemy move on the passed direction
         k.onUpdate(() => {
             enemy.move(enemy.dir * enemy.speed, 0);
@@ -12,10 +12,13 @@ export function addEnemyIa(enemy, style) {
         // switch focus side after every 3 seconds
         loop(3, () => {
             enemy.dir = -enemy.dir;
+            enemy.flipX = !enemy.flipX;
+
         });
         // switch focus side when collide with the player
         k.onCollide('enemy', 'player', (enemy) => {
             enemy.dir = -enemy.dir;
+            enemy.flipX = !enemy.flipX;
         });
     };
 };
